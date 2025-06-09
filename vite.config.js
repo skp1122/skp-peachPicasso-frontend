b92b3c68c5dcf6203f3fe7031c575385748bad75
Suryansh Kumar <skp1108rnc@gmail.com>
2025-06-09 23:58:28 +0530

    fix: entry point as website is live

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/skp-peachPicasso-frontend/',
+  base: '/',
   plugins: [react()],
 });
